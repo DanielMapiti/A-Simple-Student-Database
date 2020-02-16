0fea2916c60a7ec0f71a6ea862192a79ba5b98e4
Daniel Mapiti <mptsan003@myuct.ac.za>
2020-02-16 02:59:20 +0200

    Created a main function and an event loop

---
+++ DbDriver.cpp
@@ -0,0 +1,15 @@
+#include "DbHeader.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int choice;
+    for(;;){
+
+        cout<<"Enter a number (or q to quit) and press return...\n";
+        cout<<"0: Add Student\n2: Read Database\n3: Save Database\n4: Display Given Student Data\n5: Grade Student\nq: Quit"
+
+    }
+    return 0;
+}
