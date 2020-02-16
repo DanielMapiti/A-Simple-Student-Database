e6a4aa907b64fe57378dc05f7e93bafde3dbd0a8
Daniel Mapiti <mptsan003@myuct.ac.za>
2020-02-16 03:10:01 +0200

    Created a makefile.

--- DbDriver.cpp
+++ DbDriver.cpp
@@ -8,7 +8,7 @@ int main(){
     for(;;){
 
         cout<<"Enter a number (or q to quit) and press return...\n";
-        cout<<"0: Add Student\n2: Read Database\n3: Save Database\n4: Display Given Student Data\n5: Grade Student\nq: Quit"
+        cout<<"0: Add Student\n2: Read Database\n3: Save Database\n4: Display Given Student Data\n5: Grade Student\nq: Quit";
 
     }
     return 0;
